2ce838a335dc4e609dcb9c433338bf1758843f7d
Teobotti <61881708+Teobotti@users.noreply.github.com>
2021-05-30 21:42:09 +0200

    prima change

--- graph.c
+++ graph.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct grafo {
+    int id;
+    int camMin;
+    struct grafo *next;
+} grafo;
 
 void fun() {
-    fputc(5,stdout);
+    fputc('5',stdout);
 }
 
-
-
 int main(int argc, char * argv[]) {
-    printf("Hello, World!\n");
-    fun();
+    int d,k;
+    int check = 0;
+    int id_grafo=0;
+    fscanf(stdin, "%d,%d", &d, &k);
+    grafo *head = NULL; //dichiaro la lista che sarà la classifica dei grafi
+
+    while(check == 0){
+        
+        check = 1;
+    }
+
     return 0;
 }
 
